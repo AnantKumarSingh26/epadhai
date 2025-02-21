078ad7bb5c6ab139eb76df73ad996c973530c106
Anant <Thalaanaaa@gmail.com>
2025-02-21 23:43:47 +0530

    Update in code login

--- estudy/lib/pages/login.dart
+++ estudy/lib/pages/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -7,18 +8,34 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Container(
-          
-          child: Center(
+        body: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: SingleChildScrollView(
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
+                SizedBox(
+                  height: 50,
+                ),
+                Align(
+                    alignment: Alignment.centerLeft,
+                    child: ColorChangingText()),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Login',
+                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                SizedBox(
+                  height: 90,
+                ),
                 Text(
                   'Username',
                   style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                 ),
                 SizedBox(
-                  height: 5,
+                  height: 15,
                 ),
                 TextField(
                   decoration: InputDecoration(
@@ -34,27 +51,30 @@ class LoginPage extends StatelessWidget {
                   ),
                 ),
                 SizedBox(
-                  height: 20,
+                  height: 40,
                 ),
                 Text(
                   'Password',
                   style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                 ),
                 SizedBox(
-                  height: 5,
+                  height: 15,
                 ),
                 PasswordField(), //Calling Stateful Password
-                TextButton(
-                    onPressed: () {
-                      print('forget button');
-                    },
-                    child: const Text(
-                      'Forgotten Password ?',
-                      style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                          color: Color.fromARGB(255, 255, 17, 0)),
-                    )),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                      onPressed: () {
+                        // print('forget button');
+                      },
+                      child: const Text(
+                        'Forgotten Password ?',
+                        style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromARGB(255, 255, 17, 0)),
+                      )),
+                ),
                 SizedBox(height: 20),
                 SizedBox(
                   width: 240,
@@ -75,16 +95,28 @@ class LoginPage extends StatelessWidget {
                 SizedBox(
                   height: 10,
                 ),
-                TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    'Don\'t have an Account?',
-                    style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.w900,
-                        color: const Color.fromARGB(255, 203, 31, 31)),
-                  ),
-                )
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Don\'t have an Account?',
+                      style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w900,
+                          color: const Color.fromARGB(206, 53, 53, 54)),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Sign-Up',
+                        style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w900,
+                            color: const Color.fromARGB(255, 203, 31, 31)),
+                      ),
+                    )
+                  ],
+                ),
               ],
             ),
           ),
@@ -93,6 +125,8 @@ class LoginPage extends StatelessWidget {
     );
   }
 }
+
+//Making Password Field Statefull
 
 class PasswordField extends StatefulWidget {
   const PasswordField({super.key});
@@ -125,6 +159,49 @@ class _PasswordFieldState extends State<PasswordField> {
             },
             icon: Icon(_obscureText ? Icons.visibility : Icons.visibility_off)),
         hintStyle: TextStyle(fontSize: 17),
+      ),
+    );
+  }
+}
+
+// Creating a separate StatefulWidget for the welcome text
+class ColorChangingText extends StatefulWidget {
+  const ColorChangingText({super.key});
+
+  @override
+  State<ColorChangingText> createState() => _ColorChangingTextState();
+}
+
+class _ColorChangingTextState extends State<ColorChangingText> {
+  bool isBlack = true;
+  late Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        isBlack = !isBlack;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'Welcome Back',
+      style: TextStyle(
+        fontSize: 50,
+        fontWeight: FontWeight.bold,
+        color: isBlack
+            ? const Color.fromARGB(255, 255, 78, 14)
+            : const Color.fromARGB(255, 8, 102, 243),
       ),
     );
   }
